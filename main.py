Fix wrong usage of tf.stop_gradient, hopefully

main.py
@@ -40,6 +40,10 @@ def load_vgg(sess, vgg_path):
     layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
     layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
 
+    tf.stop_gradient(layer3_out)
+    tf.stop_gradient(layer4_out)
+    tf.stop_gradient(layer7_out)
+    
     return input_layer, keep_prob, layer3_out, layer4_out, layer7_out
 
 tests.test_load_vgg(load_vgg, tf)
@@ -64,11 +68,11 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     The encoder for FCN-8 is the VGG16 model pretrained on ImageNet for classification.
     The fully-connected layers are replaced by 1-by-1 convolutions
     """
-    c1_1x1_layer7 = conv_1x1(tf.stop_gradient(vgg_layer7_out), num_classes)
+    c1_1x1_layer7 = conv_1x1(vgg_layer7_out, num_classes)
 
-    c1_1x1_layer4 = conv_1x1(tf.stop_gradient(vgg_layer4_out), num_classes)
+    c1_1x1_layer4 = conv_1x1(vgg_layer4_out, num_classes)
 
-    c1_1x1_layer3 = conv_1x1(tf.stop_gradient(vgg_layer3_out), num_classes)
+    c1_1x1_layer3 = conv_1x1(vgg_layer3_out, num_classes)
 
 
     #Match layer sizes (upsample) and create skip layers