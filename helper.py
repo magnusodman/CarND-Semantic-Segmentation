3c4df2444e79a1e69ce8f232bd6bf9db8fbb241d
Magnus Ödman <magnus.odman@gmail.com>
2017-10-02 20:45:01 +0200

    Remove print debug

--- helper.py
+++ helper.py
@@ -88,8 +88,6 @@ def gen_batch_function(data_folder, image_shape):
                 gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
 
                 gt_bg = np.all(gt_image == background_color, axis=2)
-                print(gt_bg.shape)
-                print(*gt_bg.shape)
                 gt_bg = gt_bg.reshape(gt_bg.shape[0], gt_bg.shape[1], 1)
                 gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
 
